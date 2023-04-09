c5a419b1677d83a76f1922aca22f07c6e9463b89
BenSolomon <bds1217@gmail.com>
2023-04-08 19:19:43 -0700

    Refactor df to drb to avoid possible namespace conflict

--- R/predict_drb345.R
+++ R/predict_drb345.R
@@ -1,6 +1,6 @@
 #' Create DRB345 read ratios
 #'
-#' @param df A data frame with columns: \cr
+#' @param drb A data frame with columns: \cr
 #' "sample" - unique sample name \cr
 #' "DRB1", "DRB3", "DRB4", "DRB5" - raw read counts for each loci
 #'
@@ -12,15 +12,15 @@
 #' data(drb345_data)
 #' hlatools:::make_drb345_ratios(drb345_data)
 #'
-make_drb345_ratios <- function(df){
+make_drb345_ratios <- function(drb){
   # Assert compatible columns in df
   arg_col <- checkmate::makeAssertCollection()
   df_columns <- c("sample", "DRB1", "DRB3", "DRB4", "DRB5")
-  checkmate::assertNames(names(df), must.include = df_columns, .var.name = "df column names", add = arg_col)
+  checkmate::assertNames(names(drb), must.include = df_columns, .var.name = "df column names", add = arg_col)
   if (arg_col$isEmpty()==F) {purrr::map(arg_col$getMessages(),print); checkmate::reportAssertions(arg_col)}
 
   # Calculate ratios
-  df %>%
+  drb %>%
     dplyr::mutate_at(dplyr::vars(DRB3:DRB5), dplyr::funs(./DRB1)) %>%
     dplyr::select(-DRB1)
 }
@@ -29,7 +29,7 @@ make_drb345_ratios <- function(df){
 
 #' Predict DRB345 copy numbers
 #'
-#' @param df A data frame with columns: \cr
+#' @param drb A data frame with columns: \cr
 #' "sample" - unique sample name \cr
 #' "DRB1", "DRB3", "DRB4", "DRB5" - raw read counts for each loci
 #'
@@ -44,19 +44,19 @@ make_drb345_ratios <- function(df){
 #' # data(drb345_data)
 #' # predict_drb345(drb345_data)
 #'
-predict_drb345 <- function(df) {
+predict_drb345 <- function(drb) {
   # Assertions for df are contained in make_drb345_ratios
 
   # Convert DRB345 counts in ratios
-  df <- make_drb345_ratios(df)
+  drb <- make_drb345_ratios(drb)
 
   # drb345_knn model is available from sysdata.rda
 
   # Format df and apply kNN
-  df <- tidyr::expand_grid(df, locus = c("DRB3", "DRB4", "DRB5")) %>%
+  drb <- tidyr::expand_grid(drb) %>%
 
-  df %>%
-    dplyr::bind_cols(stats::predict(drb345_knn, new_data = df)) %>%
+    drb %>%
+    dplyr::bind_cols(stats::predict(drb345_knn, new_data = drb)) %>%
     dplyr::mutate("copy_number" = as.numeric(as.character(.pred_class))) %>%
     dplyr::select(sample, locus, copy_number) %>%
     tidyr::pivot_wider(names_from = "locus", values_from = "copy_number")
