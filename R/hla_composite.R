50c3a06988e84cc34bf77ae3927ecba65c078f11
BenSolomon <bds1217@gmail.com>
2023-08-07 15:16:21 -0700

    Added composite genotype pipeline function

--- R/hla_composite.R
+++ R/hla_composite.R
@@ -76,3 +76,51 @@ predict_composite <- function(hla){
     tibble::rownames_to_column("sample") %>%
     dplyr::mutate(sample = gsub("_[0-9]*$", "", sample))
 }
+
+
+#' Full pipeline to predict best HLA allele from multiple genotypers
+#' using pre-trained decision tree
+#'
+#' @param df Data frame of HLA alleles genotypers as read by the read_* set of functions
+#' @param digits Which allele resolution to compile
+#'
+#' @return data frame
+#' @export
+#'
+#' @examples
+#' # No examples
+composite_pipeline <- function(df, digits = 4){
+  arg_col <- checkmate::makeAssertCollection()
+  accept_digits <- c(2,4,6)
+  checkmate::assertChoice(digits, choices = c(2,4,6), .var.name = "digit resolution", add = arg_col)
+  if (arg_col$isEmpty()==F) {map(arg_col$getMessages(),print);checkmate::reportAssertions(arg_col)}
+
+  field_select <- case_when(
+    digits == 6 ~ "field_3",
+    digits == 4 ~ "field_2",
+    digits == 2 ~ "field_1"
+  )
+
+  df <- df %>%
+    dplyr::filter(grepl("(^[ABC]|^D[PQR].*[1345]$)", locus)) %>%
+    tidyr::separate(allele, into = c("locus", "fields"), sep = "\\*",
+                    fill = "right", extra = "drop") %>%
+    tidyr::separate(fields, into = c("field_1", "field_2", "field_3"), sep = ":",
+                    fill = "right", extra = "drop") %>%
+    dplyr::mutate(field_2 = paste(field_1, field_2, sep = "_"),
+                  field_3 = paste(field_2, field_3, sep = "_")) %>%
+    dplyr::mutate_at(vars(field_1:field_3), function(x) ifelse(grepl("NA",x),NA,x)) %>%
+    tidyr::pivot_longer(contains("field"), names_to = "field", values_to = "allele")
+
+  df %>%
+    dplyr::left_join(
+      predict_composite(df),
+      by = c("sample", "locus", "field")) %>%
+    dplyr::filter(genotyper == best_genotyper) %>%
+    dplyr::select(-best_genotyper) %>%
+    dplyr::filter(field == field_select) %>%
+    dplyr::mutate(allele = gsub("_", "\\:", allele)) %>%
+    tidyr::unite(allele, locus, allele, sep = "*", remove = F) %>%
+    dplyr::select(sample, locus, allele_id, allele, genotyper) %>%
+    dplyr::arrange(sample, locus, allele_id)
+}
